92f42233c3d7391436a82513ab392fa29191a825
HMOMOKI <momoki.hiromichi@d-plustrading.com>
2022-06-28 09:40:38 +0900

    ModelにUserを保存するメソッドの記述

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Http\Request;
 
 class User extends Authenticatable
 {
@@ -40,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function store(Request $request) :void
+    {
+        $this->fill([
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        $this->save();
+    }
 }
